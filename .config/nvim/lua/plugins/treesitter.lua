c93f65ad154ddb75c6e79831f42483d992d420bf
veplix <127900312+veplix@users.noreply.github.com>
2025-12-10 23:07:04 +0100

    Update treesitter.lua

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,12 +2,13 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+
+    lazy = false,
+
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
       "windwp/nvim-ts-autotag",
     },
-    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
 
     opts = {
       ensure_installed = {
@@ -40,10 +41,8 @@ return {
         "regex",
         "gitignore",
         "dockerfile",
+        "c_sharp",
       },
-
-      sync_install = false,
-      auto_install = true,
 
       highlight = {
         enable = true,
@@ -57,28 +56,6 @@ return {
 
       autotag = {
         enable = true,
-        enable_rename = true,
-        enable_close = true,
-        enable_close_on_slash = true,
-        filetypes = {
-          "html",
-          "javascript",
-          "typescript",
-          "javascriptreact",
-          "typescriptreact",
-          "svelte",
-          "vue",
-          "tsx",
-          "jsx",
-          "rescript",
-          "xml",
-          "php",
-          "markdown",
-          "astro",
-          "glimmer",
-          "handlebars",
-          "hbs",
-        },
       },
 
       incremental_selection = {
@@ -86,7 +63,6 @@ return {
         keymaps = {
           init_selection = "<C-space>",
           node_incremental = "<C-space>",
-          scope_incremental = false,
           node_decremental = "<bs>",
         },
       },
@@ -96,81 +72,12 @@ return {
           enable = true,
           lookahead = true,
           keymaps = {
-            ["af"] = { query = "@function.outer", desc = "Select outer function" },
-            ["if"] = { query = "@function.inner", desc = "Select inner function" },
-            ["ac"] = { query = "@class.outer", desc = "Select outer class" },
-            ["ic"] = { query = "@class.inner", desc = "Select inner class" },
-            ["aa"] = { query = "@parameter.outer", desc = "Select outer parameter" },
-            ["ia"] = { query = "@parameter.inner", desc = "Select inner parameter" },
-            ["ai"] = { query = "@conditional.outer", desc = "Select outer conditional" },
-            ["ii"] = { query = "@conditional.inner", desc = "Select inner conditional" },
-            ["al"] = { query = "@loop.outer", desc = "Select outer loop" },
-            ["il"] = { query = "@loop.inner", desc = "Select inner loop" },
-            ["a/"] = { query = "@comment.outer", desc = "Select outer comment" },
-            ["ab"] = { query = "@block.outer", desc = "Select outer block" },
-            ["ib"] = { query = "@block.inner", desc = "Select inner block" },
-          },
-          selection_modes = {
-            ["@parameter.outer"] = "v",
-            ["@function.outer"] = "V",
-            ["@class.outer"] = "<c-v>",
-          },
-          include_surrounding_whitespace = false,
-        },
-
-        swap = {
-          enable = true,
-          swap_next = {
-            ["<leader>a"] = { query = "@parameter.inner", desc = "Swap parameter with next" },
-          },
-          swap_previous = {
-            ["<leader>A"] = { query = "@parameter.inner", desc = "Swap parameter with previous" },
-          },
-        },
-
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            ["]f"] = { query = "@function.outer", desc = "Next function start" },
-            ["]c"] = { query = "@class.outer", desc = "Next class start" },
-            ["]a"] = { query = "@parameter.inner", desc = "Next parameter" },
-            ["]i"] = { query = "@conditional.outer", desc = "Next conditional" },
-            ["]l"] = { query = "@loop.outer", desc = "Next loop" },
-          },
-          
-          goto_next_end = {
-            ["]F"] = { query = "@function.outer", desc = "Next function end" },
-            ["]C"] = { query = "@class.outer", desc = "Next class end" },
-          },
-          
-          goto_previous_start = {
-            ["[f"] = { query = "@function.outer", desc = "Previous function start" },
-            ["[c"] = { query = "@class.outer", desc = "Previous class start" },
-            ["[a"] = { query = "@parameter.inner", desc = "Previous parameter" },
-            ["[i"] = { query = "@conditional.outer", desc = "Previous conditional" },
-            ["[l"] = { query = "@loop.outer", desc = "Previous loop" },
-          },
-          
-          goto_previous_end = {
-            ["[F"] = { query = "@function.outer", desc = "Previous function end" },
-            ["[C"] = { query = "@class.outer", desc = "Previous class end" },
-          },
-          goto_next = {
-            ["]d"] = { query = "@conditional.outer", desc = "Next conditional" },
-          },
-          goto_previous = {
-            ["[d"] = { query = "@conditional.outer", desc = "Previous conditional" },
-          },
-        },
-
-        lsp_interop = {
-          enable = true,
-          border = "rounded",
-          floating_preview_opts = {},
-          peek_definition_code = {
-            ["<leader>df"] = { query = "@function.outer", desc = "Peek function definition" },
-            ["<leader>dc"] = { query = "@class.outer", desc = "Peek class definition" },
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
           },
         },
       },
@@ -178,6 +85,14 @@ return {
 
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
+
+      vim.defer_fn(function()
+        vim.cmd("TSEnable highlight")
+      end, 20)
+
+      if vim.lsp.semantic_tokens then
+        vim.lsp.semantic_tokens.enabled = false
+      end
     end,
   },
 }
